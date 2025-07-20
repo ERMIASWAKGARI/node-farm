de86dfd007e50f14616572334ea95b2a8b8098b2
Ermias Wakgari Yadeta <ermiaswakgarisse5@gmail.com>
2025-07-20 15:32:21 +0300

    html templating: filling templates

--- index.js
+++ index.js
@@ -38,21 +38,63 @@ const url = require('url');
 
 /////////////////////// HTTP Module
 
+const tempOverview = fs.readFileSync(
+  `${__dirname}/templates/template-overview.html`,
+  'utf-8'
+);
+const tempCard = fs.readFileSync(
+  `${__dirname}/templates/template-card.html`,
+  'utf-8'
+);
+const tempProduct = fs.readFileSync(
+  `${__dirname}/templates/template-product.html`,
+  'utf-8'
+);
 const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, 'utf-8');
 const dataObj = JSON.parse(data);
+
+const replaceTemplate = (temp, product) => {
+  let output = temp.replace(/{%PRODUCTNAME%}/g, product.productName);
+  output = output.replace(/{%IMAGE%}/g, product.image);
+  output = output.replace(/{%FROM%}/g, product.from);
+  output = output.replace(/{%NUTRIENTS%}/g, product.nutrients);
+  output = output.replace(/{%QUANTITY%}/g, product.quantity);
+  output = output.replace(/{%PRICE%}/g, product.price);
+  output = output.replace(/{%DESCRIPTION%}/g, product.description);
+  output = output.replace(/{%ID%}/g, product.id);
+  if (!product.organic) {
+    output = output.replace(/{%NOT_ORGANIC%}/g, 'not-organic');
+  }
+  return output;
+};
 
 const server = http.createServer((req, res) => {
   const pathName = req.url;
 
+  //overview page
   if (pathName === '/' || pathName === '/overview') {
-    res.end('<h1>This is the OVERVIEW</h1>');
+    res.writeHead(200, {
+      'Content-Type': 'text/html',
+    });
+
+    const cardsHtml = dataObj
+      .map((el) => replaceTemplate(tempCard, el))
+      .join('');
+    const output = tempOverview.replace(/{%PRODUCT_CARDS%}/g, cardsHtml);
+    res.end(output);
+
+    //product page
   } else if (pathName === '/product') {
     res.end('<h1>This is the PRODUCT</h1>');
+
+    //API
   } else if (pathName === '/api') {
     res.writeHead(200, {
       'Content-Type': 'application/json',
     });
     res.end(data);
+
+    // Not Found
   } else {
     res.writeHead(404, {
       'Content-Type': 'text/html',
